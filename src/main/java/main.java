87f18d77458dc38a9db013a30faea64a522b4139
Arajen Kajanthirabalan <arajenkajanthira@gmail.com>
2026-01-08 00:16:45 -0500

    Add logic for entering tasks and create task class

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,35 @@
+import java.util.*;
+import java.time.*;
 void main(){
+    // Use arraylist to store each task
+    ArrayList<String> tasks = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
+    int choice;
+    do{
+        System.out.println("Task Tracker");
+        System.out.println("1. Enter new tasks");
+        System.out.println("2. Update a task");
+        System.out.println("3. Delete a task");
+        System.out.println("4. Mark a task in-progress");
+        System.out.println("5. Mark a task done");
+        System.out.println("6. List all tasks");
+        System.out.println("7. Exit Program");
+        System.out.println("Enter a choice(1-7): ");
+        choice = input.nextInt();
 
+        if(choice == 1){
+            System.out.println("How many tasks would you like to enter?");
+            int numTasks = input.nextInt();
+            for(int i = 0; i <= numTasks; i++){
+                System.out.println("Enter task: ");
+                String task = input.next();
+                tasks.add(task);
+
+            }
+            System.out.println(tasks);
+        }
+        if (choice == 2){
+
+        }
+    }while(choice != 7);
 }
---
+++ src/main/java/task.java
@@ -0,0 +1,9 @@
+import java.time.*;
+public class task {
+    private int id;
+    private String description;
+    private String status;
+    private String createdAt;
+    private String updatedAt;
+
+}
