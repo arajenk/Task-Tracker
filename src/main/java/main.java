18d8e01cbc76bc02c8ef7d6aaafe1ec7b4ea2268
Arajen Kajanthirabalan <arajenkajanthira@gmail.com>
2026-01-08 22:30:40 -0500

    Add functionality for marking task in progress, complete, and to list all tasks

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -11,7 +11,7 @@ void main(){
         System.out.println("2. Update a task");
         System.out.println("3. Delete a task");
         System.out.println("4. Mark a task in-progress");
-        System.out.println("5. Mark a task done");
+        System.out.println("5. Mark a task completed");
         System.out.println("6. List all tasks");
         System.out.println("7. Exit Program");
         System.out.println("Enter a choice(1-7): ");
@@ -45,6 +45,39 @@ void main(){
             input.nextLine();
             if(deleteIndex >= 1 && deleteIndex <= tasks.size()){
                 tasks.remove(deleteIndex-1);
+            }
+        }
+        if(choice == 4){
+            System.out.println("Select a task to mark in-progress: ");
+            int inProgressTaskId = input.nextInt();
+            input.nextLine();
+            for(Task task : tasks){
+                if(task.getId()==inProgressTaskId){
+                    task.setStatus("in-progress");
+                    System.out.println("Task status set to in-progress.");
+                }
+            }
+        }
+        if(choice == 5){
+            System.out.println("Select a task to mark complete: ");
+            int completeTaskId = input.nextInt();
+            input.nextLine();
+            for(Task task : tasks){
+                if(task.getId()==completeTaskId){
+                    task.setStatus("complete");
+                    System.out.println("Task status set to complete.");
+                }
+            }
+        }
+        if(choice == 6){
+            if (tasks.isEmpty()) {
+                System.out.println("No tasks found.");
+            }
+            else {
+                System.out.println("Tasks: ");
+                for (Task task : tasks) {
+                    System.out.println("ID: " + task.getId() + ", Description: " + task.getDescription() + ", Status: " + task.getStatus());
+                }
             }
         }
 
